cb25a1b3a288c45c489f237d2bc6bb6b5b333bc1
zainmustafai <zforzain2000@gmail.com>
2023-08-08 07:59:52 -0700

    latest changes ooooffice

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react'
-import './Navbar.css'
-import { GiHamburgerMenu } from 'react-icons/gi';
-import {CgClose} from 'react-icons/cg';
+import React, { useState } from "react";
+import "./Navbar.css";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { CgClose } from "react-icons/cg";
 import { motion } from "framer-motion"; // Animation Hooks.
 
 const scrollToSection = (ref) => {
@@ -12,61 +12,70 @@ const scrollToSection = (ref) => {
 };
 
 const openWhatsapp = () => {
-  window.open('https://wa.me/+923094272562', '_blank', 'noopener,noreferrer'); // open link in new tab.
-}
+  window.open("https://wa.me/+923094272562", "_blank", "noopener,noreferrer"); // open link in new tab.
+};
 
 const Navbar = ({ homeRef, contactRef, portfolioRef, aboutRef }) => {
   const [showSmall, setShowSmall] = useState(false);
   const handleClick = () => {
     setShowSmall(!showSmall);
-  }
+  };
   const menuItems = [
-    { text: 'HOME', id: '/hero', ref: homeRef },
-    { text: 'ABOUT', id: '/about', ref: aboutRef },
-    { text: 'PORTFOLIO', id: '/portfolio', ref: portfolioRef },
+    { text: "HOME", id: "/hero", ref: homeRef },
+    { text: "ABOUT", id: "/about", ref: aboutRef },
+    { text: "PORTFOLIO", id: "/portfolio", ref: portfolioRef },
     // {text:'BLOG',id: '#blog'},
     // { text: 'CONTACT', id: '#contact', ref: contactRef },
-  ]
+  ];
   return (
     <div>
-      <motion.div className='nav-container w-full py-3  md:pl-8 lg:pl-11 xl:pl-14 flex items-center justify-between shadow-lg'>
-        <ul className=' menu text-xl text-white font-medium hidden md:flex  sm:gap-6 md:gap-8 lg:gap-10 '>
-          {
-            menuItems.map((item) => {
-              return <motion.div
+      <motion.div className="nav-container w-full py-3  md:pl-8 lg:pl-11 xl:pl-14 flex items-center justify-between shadow-lg">
+        <ul className=" menu text-xl text-white font-medium hidden md:flex  sm:gap-6 md:gap-8 lg:gap-10 ">
+          {menuItems.map((item) => {
+            return (
+              <motion.div
                 key={item.id}
-                whileHover={{ scale: 1.1, }}
+                whileHover={{ scale: 1.1 }}
                 whileTap={{ scale: 0.9 }}
               >
-                <li className='hover:text-yellow-600'>
-                  <button key={item.id}
+                <li className="hover:text-yellow-600">
+                  <button
+                    key={item.id}
                     onClick={() => scrollToSection(item.ref)}
                   >
                     {item.text}
                   </button>
                 </li>
               </motion.div>
-            })
-          }
-          <motion.div
-            whileHover={{ scale: 1.1, }}
-            whileTap={{ scale: 0.9 }}
-          >
-            <li onClick={openWhatsapp} className='hover:text-yellow-600' >CONTACT</li>
+            );
+          })}
+          <motion.div whileHover={{ scale: 1.1 }} whileTap={{ scale: 0.9 }}>
+            <li onClick={openWhatsapp} className="hover:text-yellow-600">
+              CONTACT
+            </li>
           </motion.div>
         </ul>
-        <button className='text-white text-2xl mx-4 md:hidden cursor-pointer' onClick={handleClick}>
-          {showSmall ? <CgClose color='#fff' /> : <GiHamburgerMenu />}
+        <button
+          className="text-white text-2xl mx-4 md:hidden cursor-pointer"
+          onClick={handleClick}
+        >
+          {showSmall ? <CgClose color="#fff" /> : <GiHamburgerMenu />}
         </button>
       </motion.div>
-      
+
       {/* Small Screen Menu */}
       {showSmall && (
         <div className="md:hidden px-4">
           <ul className="flex flex-col gap-1 bg-[#101720] text-white rounded-lg animate-fadeIn">
             {menuItems.map((item) => (
               <li key={item.id} className="hover:text-yellow-600">
-                <button onClick={() => scrollToSection(item.ref)} className="p-2 w-full">
+                <button
+                  onClick={() => {
+                    scrollToSection(item.ref);
+                    handleClick();
+                  }}
+                  className="p-2 w-full"
+                >
                   {item.text}
                 </button>
               </li>
@@ -78,7 +87,7 @@ const Navbar = ({ homeRef, contactRef, portfolioRef, aboutRef }) => {
         </div>
       )}
     </div>
-  )
-}
+  );
+};
 
 export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   theme: {
     extend: {
       animation: {
-        fadeIn: "fadeIn 1s ease-in-out", // Define the fadeIn animation
+        fadeIn: "fadeIn 0.3s ease-in-out", // Define the fadeIn animation
       },
       keyframes: {
         fadeIn: {
