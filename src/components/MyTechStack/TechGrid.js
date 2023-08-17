c7347c71b7cadcfbc67e85bd07cea809dac34157
zainmustafai <zforzain2000@gmail.com>
2023-08-17 12:02:05 -0700

    Animation

--- src/components/MyTechStack/TechGrid.js
+++ src/components/MyTechStack/TechGrid.js
@@ -4,7 +4,7 @@ import StackDB from "./StackDB.js";
 const TechGrid = () => {
   return (
     <div className="w-full mx-auto px-4 sm:px-8 md:px-16 lg:px-20 xl:px-24">
-      <ul className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-8 mt-8 sm:mt-12 md:mt-16 lg:mt-20 xl:mt-24">
+      <ul className="mx-auto grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-8 mt-8 sm:mt-12 md:mt-16 lg:mt-20 xl:mt-24">
         {StackDB.map((item) => {
           return (
             <li key={item.id} className="flex items-center justify-center">
